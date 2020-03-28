e14d5e0e184aff1e717337eaf19dda2f398a3ef2
Ilya <ilyailyukou@gmail.com>
2020-03-28 19:35:15 +0300

    init spring boot project

---
+++ src/main/java/org/telegram/bot/beldtp/BeldtpApplication.java
@@ -0,0 +1,13 @@
+package org.telegram.bot.beldtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeldtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeldtpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/telegram/bot/beldtp/BeldtpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.telegram.bot.beldtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeldtpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
