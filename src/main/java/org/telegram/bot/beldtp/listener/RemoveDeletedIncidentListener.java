07215bb3caaaebbe8806686ccb6d6bc24eb69282
Ilya <ilyailyukou@gmail.com>
2020-05-05 12:51:24 +0300

    all urls wrapping on HTML

--- src/main/java/org/telegram/bot/beldtp/listener/RemoveDeletedIncidentListener.java
+++ src/main/java/org/telegram/bot/beldtp/listener/RemoveDeletedIncidentListener.java
@@ -5,7 +5,10 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.telegram.bot.beldtp.model.Incident;
 import org.telegram.bot.beldtp.model.IncidentType;
+import org.telegram.bot.beldtp.model.User;
 import org.telegram.bot.beldtp.service.interf.model.IncidentService;
+import org.telegram.bot.beldtp.service.interf.model.LocationService;
+import org.telegram.bot.beldtp.service.interf.model.UserService;
 
 @Component
 public class RemoveDeletedIncidentListener {
@@ -15,10 +18,24 @@ public class RemoveDeletedIncidentListener {
     @Autowired
     private IncidentService incidentService;
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private LocationService locationService;
+
     @Scheduled(fixedRate = REPEAT_TIME)
     public void publishIncident() {
 
         for (Incident incident : incidentService.get(IncidentType.DELETE)){
+            if(incident.getLocation() != null){
+                locationService.delete(incident.getLocation());
+            }
+            User user = incident.getUser();
+
+            user.remove(incident);
+            user = userService.save(user);
+
             incidentService.delete(incident);
         }
     }
